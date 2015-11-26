5f73b0a793a2ae755fba4387741ec7f8685163c3
maiconskyy <dutra.maicon@gmail.com>
2015-11-26 13:36:18 -0200

    Maicon
    login

--- Restauradora/Restauradora/Login.Designer.cs
+++ Restauradora/Restauradora/Login.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login));
             this.tbxEmpresa = new System.Windows.Forms.TextBox();
             this.lblEmpresa = new System.Windows.Forms.Label();
             this.lblLogin = new System.Windows.Forms.Label();
@@ -36,28 +37,32 @@
             this.tbxSenha = new System.Windows.Forms.TextBox();
             this.btnOK = new System.Windows.Forms.Button();
             this.btnCancelar = new System.Windows.Forms.Button();
+            this.pbEmpresaLogo = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbEmpresaLogo)).BeginInit();
             this.SuspendLayout();
             // 
             // tbxEmpresa
             // 
-            this.tbxEmpresa.Location = new System.Drawing.Point(93, 30);
+            this.tbxEmpresa.Location = new System.Drawing.Point(80, 31);
             this.tbxEmpresa.Name = "tbxEmpresa";
             this.tbxEmpresa.Size = new System.Drawing.Size(177, 20);
             this.tbxEmpresa.TabIndex = 0;
+            this.tbxEmpresa.Visible = false;
             // 
             // lblEmpresa
             // 
             this.lblEmpresa.AutoSize = true;
-            this.lblEmpresa.Location = new System.Drawing.Point(36, 33);
+            this.lblEmpresa.Location = new System.Drawing.Point(23, 34);
             this.lblEmpresa.Name = "lblEmpresa";
             this.lblEmpresa.Size = new System.Drawing.Size(51, 13);
             this.lblEmpresa.TabIndex = 1;
             this.lblEmpresa.Text = "Empresa:";
+            this.lblEmpresa.Visible = false;
             // 
             // lblLogin
             // 
             this.lblLogin.AutoSize = true;
-            this.lblLogin.Location = new System.Drawing.Point(36, 59);
+            this.lblLogin.Location = new System.Drawing.Point(23, 60);
             this.lblLogin.Name = "lblLogin";
             this.lblLogin.Size = new System.Drawing.Size(36, 13);
             this.lblLogin.TabIndex = 3;
@@ -65,7 +70,7 @@
             // 
             // tbxLogin
             // 
-            this.tbxLogin.Location = new System.Drawing.Point(93, 56);
+            this.tbxLogin.Location = new System.Drawing.Point(80, 57);
             this.tbxLogin.Name = "tbxLogin";
             this.tbxLogin.Size = new System.Drawing.Size(177, 20);
             this.tbxLogin.TabIndex = 2;
@@ -73,7 +78,7 @@
             // lblSenha
             // 
             this.lblSenha.AutoSize = true;
-            this.lblSenha.Location = new System.Drawing.Point(36, 85);
+            this.lblSenha.Location = new System.Drawing.Point(23, 86);
             this.lblSenha.Name = "lblSenha";
             this.lblSenha.Size = new System.Drawing.Size(41, 13);
             this.lblSenha.TabIndex = 5;
@@ -81,14 +86,14 @@
             // 
             // tbxSenha
             // 
-            this.tbxSenha.Location = new System.Drawing.Point(93, 82);
+            this.tbxSenha.Location = new System.Drawing.Point(80, 83);
             this.tbxSenha.Name = "tbxSenha";
             this.tbxSenha.Size = new System.Drawing.Size(177, 20);
             this.tbxSenha.TabIndex = 4;
             // 
             // btnOK
             // 
-            this.btnOK.Location = new System.Drawing.Point(93, 148);
+            this.btnOK.Location = new System.Drawing.Point(80, 131);
             this.btnOK.Name = "btnOK";
             this.btnOK.Size = new System.Drawing.Size(72, 23);
             this.btnOK.TabIndex = 6;
@@ -98,7 +103,7 @@
             // 
             // btnCancelar
             // 
-            this.btnCancelar.Location = new System.Drawing.Point(195, 148);
+            this.btnCancelar.Location = new System.Drawing.Point(182, 131);
             this.btnCancelar.Name = "btnCancelar";
             this.btnCancelar.Size = new System.Drawing.Size(75, 23);
             this.btnCancelar.TabIndex = 7;
@@ -106,12 +111,22 @@
             this.btnCancelar.UseVisualStyleBackColor = true;
             this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
             // 
+            // pbEmpresaLogo
+            // 
+            this.pbEmpresaLogo.Image = ((System.Drawing.Image)(resources.GetObject("pbEmpresaLogo.Image")));
+            this.pbEmpresaLogo.Location = new System.Drawing.Point(39, 5);
+            this.pbEmpresaLogo.Name = "pbEmpresaLogo";
+            this.pbEmpresaLogo.Size = new System.Drawing.Size(218, 42);
+            this.pbEmpresaLogo.TabIndex = 8;
+            this.pbEmpresaLogo.TabStop = false;
+            // 
             // Login
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.ClientSize = new System.Drawing.Size(311, 183);
+            this.ClientSize = new System.Drawing.Size(293, 179);
+            this.Controls.Add(this.pbEmpresaLogo);
             this.Controls.Add(this.btnCancelar);
             this.Controls.Add(this.btnOK);
             this.Controls.Add(this.lblSenha);
@@ -124,6 +139,7 @@
             this.Name = "Login";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Login";
+            ((System.ComponentModel.ISupportInitialize)(this.pbEmpresaLogo)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -139,5 +155,6 @@
         private System.Windows.Forms.TextBox tbxSenha;
         private System.Windows.Forms.Button btnOK;
         private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.PictureBox pbEmpresaLogo;
     }
 }
