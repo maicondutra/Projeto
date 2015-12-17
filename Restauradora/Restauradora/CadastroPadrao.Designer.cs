5e54c8c98ef632a7c3e9cdb39091328fe8a11493
maiconskyy <dutra.maicon@gmail.com>
2015-12-17 13:55:48 -0200

    Maicon
    add

---
+++ Restauradora/Restauradora/CadastroPadrao.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace Restauradora
+{
+    partial class CadastroPadrao
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CadastroPadrao));
+            this.pControl = new System.Windows.Forms.Panel();
+            this.dgvCadastro = new System.Windows.Forms.DataGridView();
+            this.btnNovo = new System.Windows.Forms.Button();
+            this.pControl.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCadastro)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pControl
+            // 
+            this.pControl.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.pControl.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pControl.Controls.Add(this.btnNovo);
+            this.pControl.Location = new System.Drawing.Point(1, 2);
+            this.pControl.Name = "pControl";
+            this.pControl.Size = new System.Drawing.Size(534, 85);
+            this.pControl.TabIndex = 0;
+            // 
+            // dgvCadastro
+            // 
+            this.dgvCadastro.AllowUserToOrderColumns = true;
+            this.dgvCadastro.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgvCadastro.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvCadastro.Location = new System.Drawing.Point(1, 93);
+            this.dgvCadastro.Name = "dgvCadastro";
+            this.dgvCadastro.Size = new System.Drawing.Size(534, 186);
+            this.dgvCadastro.TabIndex = 1;
+            // 
+            // btnNovo
+            // 
+            this.btnNovo.Location = new System.Drawing.Point(3, 3);
+            this.btnNovo.Name = "btnNovo";
+            this.btnNovo.Size = new System.Drawing.Size(79, 79);
+            this.btnNovo.TabIndex = 0;
+            this.btnNovo.UseVisualStyleBackColor = true;
+            // 
+            // CadastroPadrao
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(536, 281);
+            this.Controls.Add(this.dgvCadastro);
+            this.Controls.Add(this.pControl);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "CadastroPadrao";
+            this.Text = "Cadastro Padrão";
+            this.pControl.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCadastro)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pControl;
+        private System.Windows.Forms.DataGridView dgvCadastro;
+        private System.Windows.Forms.Button btnNovo;
+    }
+}
