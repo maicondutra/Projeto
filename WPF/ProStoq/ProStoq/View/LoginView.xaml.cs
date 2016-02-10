f024f4c160e6e63b2bf053a3923275fef6a5d6bc
maiconskyy <dutra.maicon@gmail.com>
2016-02-10 08:14:51 -0200

    Maicon

---
+++ WPF/ProStoq/ProStoq/View/LoginView.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using ProStoq.ViewModel;
+
+namespace ProStoq.View
+{
+    /// <summary>
+    /// Interaction logic for Login.xaml
+    /// </summary>
+    public partial class LoginView : LoginViewModel
+    {
+        public LoginView()
+        {
+            InitializeComponent();
+            IniciaVariaveis();
+        }
+
+        private void IniciaVariaveis()
+        {
+            base.tbxLogin = tbxLogin.Text;
+            base.tbxSenha = tbxSenha.Text;
+            if(cbxLembrar.IsChecked == true)
+            {
+                base.cbxLembrar = true;
+            }
+            else
+            {
+                base.cbxLembrar = false;
+            }
+                
+        }
+
+        private void lblFechar_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void btnLoginSistema_Click(object sender, RoutedEventArgs e)
+        {
+            ValidarUsuarioSenha();
+        }
+    }
+}
---
+++ WPF/ProStoq/ProStoq/ViewModel/LoginViewModel.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Markup;
+using System.ComponentModel;
+using System.IO;
+
+namespace ProStoq.ViewModel
+{
+    public class LoginViewModel : Window
+    {
+        public string tbxLogin;
+        public string tbxSenha;
+        public bool logado;
+        public bool cbxLembrar;
+
+        public void ValidarUsuarioSenha()
+        {
+            string login = tbxLogin;
+            string senha = FunGer.validCryptographyPass(tbxSenha);
+            string loginBanco = "";
+            string senhaBanco = "";
+
+            while (FunGer.selectDB2("SELECT nome FROM USUARIO WHERE nome = '" + login + "' AND senha = '" + senha + "'").Read())
+            {
+                loginBanco = FunGer.selectDB("SELECT nome FROM USUARIO WHERE nome = '" + login + "' AND senha = '" + senha + "'").Rows[0]["nome"].ToString();
+                break;
+            }
+            while (FunGer.selectDB2("SELECT senha FROM USUARIO WHERE nome = '" + login + "' AND senha = '" + senha + "'").Read())
+            {
+                senhaBanco = FunGer.selectDB("SELECT senha FROM USUARIO WHERE nome = '" + login + "' AND senha = '" + senha + "'").Rows[0]["senha"].ToString();
+                break;
+            }
+
+
+            //Comparando as informações
+            if ((login != "") && (senha != ""))
+            {
+                if ((login == loginBanco) && (senha == senhaBanco))
+                {
+                    // this.DialogResult = DialogResult.OK;
+                    logado = true;
+                }
+                else
+                {
+                    MessageBox.Show("Usuário ou Senha inválidos!", "Verifique os Campos", MessageBoxButton.OK, MessageBoxImage.Error);
+                    //tbxLogin.Clear();
+                    //tbxSenha.Clear();
+                    //tbxLogin.Focus();
+                    logado = false;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Insira Login e Senha!", "Verifique os Campos", MessageBoxButton.OK, MessageBoxImage.Warning);
+                //tbxLogin.Focus();
+                logado = false;
+            }
+
+        }
+
+        public void CriaUsuarioAdmin()
+        {
+            bool senhaBanco;
+            senhaBanco = FunGer.selectDB2("SELECT id FROM USUARIO").Read();
+
+            if (!senhaBanco)
+            {
+                string id = FunGer.selectDB("SELECT MAX(id) as id FROM USUARIO").Rows[0]["id"].ToString();
+                if (id == "") { id = "0"; }
+                int idconv = Convert.ToInt32(id) + 1;
+
+                FunGer.ExecutaSQL("INSERT INTO USUARIO (id,nome,senha,ativo,email,datanacimento,datahora) VALUES ('" + idconv + "','root','" + FunGer.cryptographyPass("root") + "',1,'','','" + DateTime.Now + "')");
+            }
+        }
+
+        private void SalvaUsuario()
+        {
+            if (cbxLembrar == true)
+            {
+                ZeraLogin();
+                FunGer.ExecutaSQL("UPDATE USUARIO SET lembralogin = 1 WHERE  nome = '" + tbxLogin + "' AND senha = '" + FunGer.cryptographyPass(tbxSenha) + "'");
+            }
+            else
+            {
+                ZeraLogin();
+            }
+
+        }
+
+        private void LerUsuarioSalvo()
+        {
+            try
+            {
+                tbxLogin = FunGer.selectDB("SELECT nome FROM USUARIO WHERE lembralogin = 1").Rows[0]["nome"].ToString();
+                cbxLembrar = true;
+                //tbxSenha.Focus();
+                //  tbxSenha.Select();
+            }
+            catch
+            {
+                tbxLogin = "";
+            }
+        }
+
+        private void ZeraLogin()
+        {
+            FunGer.ExecutaSQL("UPDATE USUARIO SET lembralogin = 0");
+        }
+    }
+}
