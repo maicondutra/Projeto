f32643b7c1c85c8f3be659489243357c3aa55b29
maiconskyy <dutra.maicon@gmail.com>
2016-02-22 21:36:52 -0300

    Maicon

--- WPF/ProStoq/ProStoq/View/MenuStoqView.xaml.cs
+++ WPF/ProStoq/ProStoq/View/MenuStoqView.xaml.cs
@@ -23,8 +23,68 @@ namespace ProStoq.View
         public MenuStoqView()
         {
             InitializeComponent();
-            IniciaTabs();
+            this.DataContext = this;
+
+            // IniciaTabs();
         }
+
+        #region --- CloseCommand ---
+
+        private Utils.RelayCommand _cmdCloseCommand;
+        ///
+        /// Returns a command that closes a TabItem.
+        ///
+        public ICommand CloseCommand
+        {
+            get
+            {
+                if (_cmdCloseCommand == null)
+                {
+                    _cmdCloseCommand = new Utils.RelayCommand(
+                        param => this.CloseTab_Execute(param),
+                        param => this.CloseTab_CanExecute(param)
+                        );
+                }
+                return _cmdCloseCommand;
+            }
+        }
+
+        ///
+        /// Called when the command is to be executed.
+        ///
+        /// <param name="parm">
+        /// The TabItem in which the Close-button was clicked.
+        ///
+        private void CloseTab_Execute(object parm)
+        {
+            TabItem ti = parm as TabItem;
+            if (ti != null)
+                ti.Items.Remove(parm);
+        }
+
+        ///
+        /// Called when the availability of the Close command needs to be determined.
+        ///
+        /// <param name="parm">
+        /// The TabItem for which to determine the availability of the Close-command.
+        ///
+        private bool CloseTab_CanExecute(object parm)
+        {
+            //For the sample, the closing of TabItems will only be
+            //unavailable for disabled TabItems and the very first TabItem.
+            TabItem ti = parm as TabItem;
+            if (ti != null && ti != tc.Items[0])
+                //We have a valid reference to a TabItem, so return
+                //true if the TabItem is enabled.
+                return ti.IsEnabled;
+
+            //If no reference to a TabItem could be obtained, the command
+            //cannot be executed
+            return false;
+        }
+
+        #endregion
+
 
         public void IniciaTabs()
         {
