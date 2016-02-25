5c6b355bbf93d4dbe445416f0c9e1ac564ffd889
maiconskyy <dutra.maicon@gmail.com>
2016-02-25 14:36:05 -0300

    Maicon

--- WPF/ProStoq/ProStoq/View/MenuStoqView.xaml.cs
+++ WPF/ProStoq/ProStoq/View/MenuStoqView.xaml.cs
@@ -28,62 +28,62 @@ namespace ProStoq.View
             // IniciaTabs();
         }
 
-        #region --- CloseCommand ---
+        //#region --- CloseCommand ---
 
-        private Utils.RelayCommand _cmdCloseCommand;
-        ///
-        /// Returns a command that closes a TabItem.
-        ///
-        public ICommand CloseCommand
-        {
-            get
-            {
-                if (_cmdCloseCommand == null)
-                {
-                    _cmdCloseCommand = new Utils.RelayCommand(
-                        param => this.CloseTab_Execute(param),
-                        param => this.CloseTab_CanExecute(param)
-                        );
-                }
-                return _cmdCloseCommand;
-            }
-        }
+        //private Utils.RelayCommand _cmdCloseCommand;
+        /////
+        ///// Returns a command that closes a TabItem.
+        /////
+        //public ICommand CloseCommand
+        //{
+        //    get
+        //    {
+        //        if (_cmdCloseCommand == null)
+        //        {
+        //            _cmdCloseCommand = new Utils.RelayCommand(
+        //                param => this.CloseTab_Execute(param),
+        //                param => this.CloseTab_CanExecute(param)
+        //                );
+        //        }
+        //        return _cmdCloseCommand;
+        //    }
+        //}
 
-        ///
-        /// Called when the command is to be executed.
-        ///
-        /// <param name="parm">
-        /// The TabItem in which the Close-button was clicked.
-        ///
-        private void CloseTab_Execute(object parm)
-        {
-            TabItem ti = parm as TabItem;
-            if (ti != null)
-                ti.Items.Remove(parm);
-        }
+        /////
+        ///// Called when the command is to be executed.
+        /////
+        ///// <param name="parm">
+        ///// The TabItem in which the Close-button was clicked.
+        /////
+        //private void CloseTab_Execute(object parm)
+        //{
+        //    TabItem ti = parm as TabItem;
+        //    if (ti != null)
+        //        ti.Items.Remove(parm);
+        //}
 
-        ///
-        /// Called when the availability of the Close command needs to be determined.
-        ///
-        /// <param name="parm">
-        /// The TabItem for which to determine the availability of the Close-command.
-        ///
-        private bool CloseTab_CanExecute(object parm)
-        {
-            //For the sample, the closing of TabItems will only be
-            //unavailable for disabled TabItems and the very first TabItem.
-            TabItem ti = parm as TabItem;
-            if (ti != null && ti != tc.Items[0])
-                //We have a valid reference to a TabItem, so return
-                //true if the TabItem is enabled.
-                return ti.IsEnabled;
+        /////
+        ///// Called when the availability of the Close command needs to be determined.
+        /////
+        ///// <param name="parm">
+        ///// The TabItem for which to determine the availability of the Close-command.
+        /////
+        //private bool CloseTab_CanExecute(object parm)
+        //{
+        //    //For the sample, the closing of TabItems will only be
+        //    //unavailable for disabled TabItems and the very first TabItem.
+        //    TabItem ti = parm as TabItem;
+        //    if (ti != null && ti != tc.Items[0])
+        //        //We have a valid reference to a TabItem, so return
+        //        //true if the TabItem is enabled.
+        //        return ti.IsEnabled;
 
-            //If no reference to a TabItem could be obtained, the command
-            //cannot be executed
-            return false;
-        }
+        //    //If no reference to a TabItem could be obtained, the command
+        //    //cannot be executed
+        //    return false;
+        //}
 
-        #endregion
+        //#endregion
 
 
         public void IniciaTabs()
@@ -110,10 +110,44 @@ namespace ProStoq.View
 
         private void Veiculo_Click(object sender, RoutedEventArgs e)
         {
-            ClosableTab theTabItem = new ClosableTab();
-            theTabItem.Title = "Small title";
-            tbcPadrao.Items.Add(theTabItem);
-            theTabItem.Focus();
+            //ClosableTab theTabItem = new ClosableTab();
+            //theTabItem.Title = "Nova tab";
+            //theTabItem.Content = "";
+            //tbcPadrao.Items.Add(theTabItem);
+            //theTabItem.Focus();
+
+            StackPanel st = new StackPanel();
+            st.Orientation = Orientation.Horizontal;
+            TextBlock txtb = new TextBlock();
+            txtb.Text = "New Tab";
+            txtb.Margin = new Thickness(1, 1, 1, 1);
+            txtb.VerticalAlignment = VerticalAlignment.Center;
+            st.Children.Add(txtb);
+            Button btn = new Button();
+            btn.Content = "X";
+            st.Children.Add(btn);
+
+            TabItem tbitem = new TabItem();
+            // Set the header to the stack panel with the 
+            // TextBlock and Button
+            tbitem.Header = st;
+
+            
+            var grid = new Grid();
+            ColumnDefinition col1 = new ColumnDefinition();
+            col1.Width = GridLength.Auto;
+            ColumnDefinition col2 = new ColumnDefinition();
+            col2.Width = new GridLength(1, GridUnitType.Star);
+
+            grid.ColumnDefinitions.Add(col1);
+            grid.ColumnDefinitions.Add(col2);
+
+            // This is where you define the content
+            // of the tab page. Here I just added a Grid 
+            // as an example.
+            tbitem.Content = grid;
+
+            tbcPadrao.Items.Add(tbitem);
         }
     }
 }
