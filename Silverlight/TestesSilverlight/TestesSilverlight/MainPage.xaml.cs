51321c8ac14e9656d2f09e5a9366dae40ba3aaa8
maiconskyy <dutra.maicon@gmail.com>
2016-05-27 08:36:03 -0300

    teste Silverlight

---
+++ Silverlight/TestesSilverlight/TestesSilverlight/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace TestesSilverlight
+{
+    public partial class MainPage : UserControl
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            acc.IsSelected = AccordionVisible;
+        }
+
+        public bool AccordionVisible
+        {
+            get { return true;  }
+        }
+    }
+}
